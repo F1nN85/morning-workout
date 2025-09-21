83d48637b0671b440d5999aa6791164a8f642dae
F1nN85 <vitali.avdeev@hotmail.com>
2025-09-21 14:48:44 +0300

    Add files via upload

---
+++ plank.js
@@ -0,0 +1,26 @@
+const timer = 1;
+let amountTime = timer * 30;
+
+function calculateTime() {
+    const countdown = document.querySelector('#countdown');
+    let minutes = Math.floor(amountTime/60);
+    let seconds = amountTime%60;
+
+    if (seconds <10) {
+        seconds = "0" + seconds;
+    }
+
+    countdown.textContent = `${seconds}`;
+    amountTime--;
+
+    if (amountTime < 0) {
+        stopTimer();
+        amountTime = 0;
+    }
+
+    function stopTimer() {
+        clearInterval(timerId);
+    }
+}
+
+let timerId = setInterval(calculateTime, 1000);
